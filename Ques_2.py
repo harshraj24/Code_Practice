a3a750a8ca919320b68e8509083f5d40d9611571
Bharati Gupta <bharatigupta2405@gmail.com>
2020-08-29 20:04:06 +0530

    Update Ques_2.py

--- Ques_2.py
+++ Ques_2.py
@@ -1,3 +1,9 @@
 # WAP to input an array seperated by space ' ' and find it's 2nd max item without using max Function
 # Input : 1 3 4 2 5 8 6 6 4 9 8
 # Output : 8
+n=input()
+lst=n.split(" ")
+li=[]
+for i in lst:
+    li.append(int(i))
+print(list(sorted(list(set(li))))[-2])
