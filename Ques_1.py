fe82afb6657cec8479562efcea276f6cda4963ba
Harsh Raj <me@harshraj.xyz>
2020-08-29 13:19:16 +0530

    Added 2 Questions

---
+++ Ques_1.py
@@ -0,0 +1,4 @@
+# WAP to inpt 2 nos. sepearated by ',' & find a^b without using '**'
+# Input : 2,3
+# Output : 8
+# Explanation : 2^3
---
+++ Ques_2.py
@@ -0,0 +1,3 @@
+# WAP to input an array seperated by space ' ' and find it's 2nd max item without using max Function
+# Input : 1 3 4 2 5 8 6 6 4 9 8
+# Output : 8
