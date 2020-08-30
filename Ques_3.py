36eca970b86234d60cd1ef8a887176d110c47717
Harsh Raj <me@harshraj.xyz>
2020-08-30 13:07:50 +0530

    Added new Qustion

---
+++ Ques_3.py
@@ -0,0 +1,3 @@
+# WAP to input n elements in a list & Display the list in the sorted order without using any built in Function
+# Input : Harsh Bharati Abhimanyu Abhinav
+# Output : Abhimanyu Abhinav Bharati Harsh
