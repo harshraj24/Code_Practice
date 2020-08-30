ff1005f2deffe94650dd167af2f9d8f1036217ce
Harsh Raj <me@harshraj.xyz>
2020-08-30 13:08:34 +0530

    Added new Qustion

---
+++ Ques_4.py
@@ -0,0 +1,6 @@
+# WAP to enter n datas of students in a dictionary and diplay the 2nd position student with their average
+# Input :  5
+#          Harsh 20 30 60
+#          Bharati 50 90 70
+#          Abhimanyu 60 90 80
+# Output : Bharati 70.00
