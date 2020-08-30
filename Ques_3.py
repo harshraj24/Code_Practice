71a9651bd4b65f0f8b702b9c1a597355ef557383
Bharati Gupta <bharatigupta2405@gmail.com>
2020-08-30 17:55:28 +0530

    Update Ques_3.py

--- Ques_3.py
+++ Ques_3.py
@@ -1,3 +1,16 @@
 # WAP to input n elements in a list & Display the list in the sorted order without using any built in Function
 # Input : Harsh Bharati Abhimanyu Abhinav
 # Output : Abhimanyu Abhinav Bharati Harsh
+def quicksort(lst):
+    if not lst:
+        return []
+    return (quicksort([x for x in lst[1:] if x <  lst[0]])
+            + [lst[0]] +
+            quicksort([x for x in lst[1:] if x >= lst[0]]))
+
+n=int(input())
+lst=[]
+for i in range (n):
+  s=input()
+  lst.append(s)
+print(quicksort(lst))
